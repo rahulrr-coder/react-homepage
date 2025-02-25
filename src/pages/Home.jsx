9dcfeb1b846aac9d3643c56cf43d86936438d6a3
rahulrr-coder <rahulrr5002@protonmail.com>
2025-02-25 16:55:26 +0530

    React HomePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../App.css"
+
+const Home = () => {
+    return (
+    <div className="Home">
+   <h1> Hey Everyone, Im Rahul </h1>
+    <h2> Contact: 92497XXXXX </h2>
+    </div>
+    )};
+
+export default Home;
